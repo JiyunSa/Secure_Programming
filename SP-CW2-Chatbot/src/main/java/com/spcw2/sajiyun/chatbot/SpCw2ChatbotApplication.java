0b6feb661e83b8047c469a7da7f0af4d143d2f5b
JiyunSa <81289580+JiyunSa@users.noreply.github.com>
2021-04-18 11:11:29 +0900

    Add files via upload

---
+++ SP-CW2-Chatbot/src/main/java/com/spcw2/sajiyun/chatbot/SpCw2ChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.spcw2.sajiyun.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCw2ChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCw2ChatbotApplication.class, args);
+	}
+
+}
---
+++ SP-CW2-Chatbot/src/test/java/com/spcw2/sajiyun/chatbot/SpCw2ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spcw2.sajiyun.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCw2ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
